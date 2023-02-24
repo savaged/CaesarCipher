e5a119406b1dab03b25cacab98b61654b996401e
savaged <savagedavid@gmail.com>
2023-02-24 08:31:27 +0000

    tidy

--- Libs/Rot13.Lib/Extensions.cs
+++ Libs/Rot13.Lib/Extensions.cs
@@ -4,13 +4,13 @@ public static class Extensions
 {
     public static char[] Rot13(this string self) =>
         self.ToCharArray().Rot13();
-    
+
     public static char Rot13(this ConsoleKeyInfo self) =>
         self.KeyChar.Rot13();
 
     private static char[] Rot13(this IEnumerable<char> self) =>
         self.Select(c => c.Rot13()).ToArray();
-    
+
     private static char Rot13(this char self) =>
         self switch
         {
